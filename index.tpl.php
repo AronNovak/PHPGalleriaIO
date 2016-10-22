3591c86703b206b369f2b19cf69592b954e2f0af
Aron Novak <aron.novak@agenceinovae.com>
2016-10-22 19:38:43 +0200

    index.tpl.php: galleria.io, jquery version updates

--- index.tpl.php
+++ index.tpl.php
@@ -3,10 +3,10 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title><?= $title ?></title>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/galleria.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.7/galleria.min.js"></script>
   <link rel="stylesheet" type="text/css" href="<?= $css ?>">
-  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/themes/classic/galleria.classic.min.css">
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.7/themes/classic/galleria.classic.min.css">
   <script>var galleria_config = <?= $galleria_config ?></script>
   <script src="<?= $js ?>"></script>
 </head>
